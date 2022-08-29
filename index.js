a3f38be7dc1d1c308dcda4d338475f5719731576
Nikita Rodyukov <nikitarodwork@gmail.com>
2022-08-29 13:59:41 +0300

    added visual

--- index.js
+++ index.js
@@ -1,80 +1,121 @@
-  const wrapper = document.querySelector('.wrapper');
-  const input = wrapper.querySelector('input');
-  const sugList = wrapper.querySelector('.suggestions');
-  const addedRepos = wrapper.querySelector('.reps');
-  let data = []
+const wrapper = document.querySelector(".wrapper");
+const input = wrapper.querySelector("input");
+const sugList = wrapper.querySelector(".suggestions");
+const addedRepos = wrapper.querySelector(".reps");
+let data = [];
 
-  const debounce = (fn, debounceTime) => {
-    let timer;
-    return function () {
-      const funcCall = () => fn.apply(this, arguments);
+const debounce = (fn, debounceTime) => {
+  let timer;
+  return function () {
+    const funcCall = () => fn.apply(this, arguments);
 
-      clearTimeout(timer);
+    clearTimeout(timer);
 
-      timer = setTimeout(funcCall, debounceTime);
-    };
+    timer = setTimeout(funcCall, debounceTime);
   };
+};
 
-  let getData = async () => {
-    const response = await fetch( `https://api.github.com/search/repositories?q=${input.value}&sort=stars`)
-    data = await response.json()  
-    data = data.items
-
-    createSugList()
+let getData = async () => {
+  if (input.value.length === 0) {
+    clearSugList();
+    return;
   }
 
-  getData = debounce(getData, 200)
+  try {
+    const response = await fetch(
+      `https://api.github.com/search/repositories?q=${input.value}&sort=stars`
+    );
+    data = await response.json();
+    data = data.items;
+  } catch (e) {
+    throw new Error(e);
+  }
 
-  const createElement = (tagName, className, text) => {
-    const element = document.createElement(tagName)
-    element.classList.add(className)
-    if (text){
-      element.innerText = text
+  clearSugList();
+  createSugList();
+};
+
+getData = debounce(getData, 200);
+
+const createElement = (tagName, className, text) => {
+  const element = document.createElement(tagName);
+  element.classList.add(className);
+  if (text) {
+    element.innerText = text;
+  }
+
+  return element;
+};
+
+const clearSugList = () => {
+  while (sugList.firstChild) {
+    sugList.firstChild.remove();
+  }
+};
+
+const createSugList = () => {
+  data = data.filter((item) => item.name.includes(input.value));
+
+  for (let i = 0; i < 5; i++) {
+    const item = data[i];
+    try {
+      const sugListElement = createElement(
+        "li",
+        "suggestions__element",
+        item.name
+      );
+      sugList.appendChild(sugListElement);
+    } catch {
+      continue;
     }
-
-    return element
   }
+};
 
-  const createSugList = () => {
-    while (sugList.firstChild) {
-      sugList.firstChild.remove();
-    }
-    
-    data = data.filter((item) => item.name.includes(input.value));
-
-    for (let i = 0; i < 5; i++) {
-      const item = data[i];
-      const sugListElement = createElement('li', 'suggestions__element', item.name)
-      sugList.appendChild(sugListElement);    
-  }
-  }
-  input.addEventListener('keyup', getData)
-
-  sugList.addEventListener('click', (e) => {
+const addtoReps = (event) => {
+  {
     for (let i in data) {
-      if (data[i].name === e.target.innerText) {
+      if (data[i].name === event.innerText) {
         const target = data[i];
 
-        const repsListElement = createElement('li', 'reps__element')
+        const repsListElement = createElement("li", "reps__element");
 
-        const repoName = createElement('p', 'reps__text', `Name: ${target.name}`)
+        const repoName = createElement(
+          "p",
+          "reps__text",
+          `Name: ${target.name}`
+        );
         repsListElement.appendChild(repoName);
 
-        const repoOwner = createElement('p', 'reps__text', `Owner: ${target.owner.login}`)
+        const repoOwner = createElement(
+          "p",
+          "reps__text",
+          `Owner: ${target.owner.login}`
+        );
         repsListElement.appendChild(repoOwner);
 
-        const repoStars = createElement('p', 'reps__text', `Stars: ${target.stargazers_count}`)
-        repsListElement.appendChild(repoStars);        
+        const repoStars = createElement(
+          "p",
+          "reps__text",
+          `Stars: ${target.stargazers_count}`
+        );
+        repsListElement.appendChild(repoStars);
 
-        const deleteBtn = createElement('div', 'delete-btn', 'delete')
+        const deleteBtn = createElement("div", "delete-btn");
         repsListElement.appendChild(deleteBtn);
 
         addedRepos.appendChild(repsListElement);
 
-        deleteBtn.addEventListener('click', () => {
-          deleteBtn.closest('.reps__element').remove();
+        deleteBtn.addEventListener("click", () => {
+          deleteBtn.closest(".reps__element").remove();
         });
+
+        input.value = "";
+        clearSugList();
         break;
       }
     }
-  });
+  }
+};
+
+input.addEventListener("keyup", getData);
+sugList.addEventListener("click", (e) => addtoReps(e.target));
